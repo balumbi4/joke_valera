19eb13bf938dfe33a812bc9ff6a341f859591673
228demonik228@list.ru <228demonik228@list.ru>
2025-06-24 19:13:24 +0300

    Добавьте файлы проекта.

---
+++ log_solo/Program.cs
@@ -0,0 +1,57 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log_solo
+{
+    internal class Program
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure(new FileInfo("../../../loggerConfig.xml"));
+            _log.Info("я работаю)");
+            Out();
+        }
+        static private void Out()
+        {
+            try
+            {
+                bool f = true;
+                while (f)
+                {
+                    Console.WriteLine("52 или z?");
+                    var s = Console.ReadLine();
+                    if (s == "52")
+                    {
+                        Console.WriteLine("ты не наш... ты ошибка...");
+                        throw new Exp52("чел, ты правда выбрал 52?? Пошел нахуй!");
+                    }
+                    else if (s == "z" || s == "Z")
+                    {
+                        Console.WriteLine("ZZZZ наш слоняра ZZZ");
+                        f = false;
+                    }
+                    else Console.WriteLine("ты ахуел? заново");
+                }
+            }
+            catch (Exp52 ex)
+            {
+                _log.Error(ex.Message);
+            }
+
+        }
+    }
+    class Exp52 : Exception
+    {
+        public Exp52() : base() { }
+        public Exp52(string massage) : base(massage) { }
+    }
+}
+
